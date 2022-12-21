04782d5791a6f2ffa66da917ef86e4e073034ded
roldanmoncada <roldan.h.moncada@gmail.com>
2022-12-20 19:20:49 -0700

    beginning the homeRoutes.js file. Building the framework of the https endpoints.

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const { User, Follower, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/')
+
+module.exports = router;
