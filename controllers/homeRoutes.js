17fcc2ad2575fa9ac09c64eaaeb3ffa5c7e263d0
roldanmoncada <roldan.h.moncada@gmail.com>
2022-12-20 21:29:19 -0700

    created the projectERD file to visualize our data structure and connections.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -9,12 +9,14 @@ router.get('/', async (req,res) => {
     }
 });
 
-router.get('/login', async (req,res) => {
-    try {
-        
-    } catch (error) {
-        res.status(500).json(error);
+router.get('/login', (req,res) => {
+    // If the user is already logged in, their request is redirected to their dashboard page.
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
     }
+    // rendering the login.handlebars file if the user is not logged in.
+    res.render('login')
 });
 
 module.exports = router;
