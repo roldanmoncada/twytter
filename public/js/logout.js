2e563203c5e72ea138bf8cf790acd9f9388b30f3
Annie Li <annielawang@gmail.com>
2022-12-23 19:05:33 -0700

    updated logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,15 +1,14 @@
-// log out function
-const logout = async function () {
-  const response = await fetch("/api/users/logout", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+const logout = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
   });
 
   if (response.ok) {
-    document.location.replace("/");
+    document.location.replace('/');
   } else {
     alert(response.statusText);
   }
 };
 
-document.querySelector("#logout").addEventListener("click", logout);
+document.querySelector('#logout').addEventListener('click', logout);
