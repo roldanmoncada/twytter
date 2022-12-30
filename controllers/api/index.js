78c57914d756a4b00c1eec31f4f9fa2ba7837de0
Heidrun Kubiessa <heidrunkubiessa@Heidruns-MacBook-Air.local>
2022-12-30 16:39:17 +0100

    adjusted field attributes in controller files, added data routes files for userRoutes, postRoutes, commentRoutes, followerRoutes. followRoutes not coded yet.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,4 +1,14 @@
 const router = require("express").Router();
+const userRoutes = require('./userRoutes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
+const followerRoutes = require('./followerRoutes');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+router.use('/followers', followerRoutes);
+
 // PLACEHOLDER FOR THE CONSTS IN WHICH WE'RE STORING THE REQUIRED API ROUTES
 
 // PLACEHOLDER FOR THE ROUTER.USE LINES OF CODE
