b7085e025f674a9ff2ff43a54261ed33eeccbceb
Annie Li <annielawang@gmail.com>
2023-01-03 18:44:50 -0700

    added follow.js

---
+++ public/js/follow.js
@@ -0,0 +1,26 @@
+async function followHandler(event) {
+  event.preventDefault();
+
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  const response = await fetch(`/api/following/${id}`, {
+    method: "POST",
+    body: JSON.stringify({
+      id,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    alert("Follow successfully!");
+    // document.location.replace("/dashboard/");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector(".follow-btn").addEventListener("click", followHandler);
