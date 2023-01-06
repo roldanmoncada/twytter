c4c80210cfdd43988599f6b46c1cec56e626987b
Annie Li <annielawang@gmail.com>
2023-01-05 19:05:54 -0700

    added helper function isequal

--- utils/helper.js
+++ utils/helper.js
@@ -1,2 +1,9 @@
 // PLACEHOLDER FOR ANY HELPERS WE CREATE
-
+module.exports = {
+  ifEquals: (a, b, options) => {
+    if (a == b) {
+      return options.fn(this);
+    }
+    return options.inverse(this);
+  },
+};
