35d1f97dadd76827f02da63544df9e370cc9445c
roldanmoncada <roldan.h.moncada@gmail.com>
2023-01-05 19:56:38 -0700

    updating to switch branchces.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -74,13 +74,13 @@ router.get("/signup", (req, res) => {
   res.render("signup");
 });
 
-router.get("/signup", (req, res) => {
-  if (req.session.logged_in) {
-    res.redirect("/");
-    return;
-  }
-  res.render("signup");
-});
+// router.get("/signup", (req, res) => {
+//   if (req.session.logged_in) {
+//     res.redirect("/");
+//     return;
+//   }
+//   res.render("signup");
+// });
 
 router.get("/post/:id", withAuth, (req, res) => {
   Post.findOne({
